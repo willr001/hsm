32179a38493c8a0c74cdc841f672f199a033f405
Will Reynolds <will.rnlds@gmail.com>
2019-10-22 06:15:14 -0500

    no idea

--- src/Hsm_ProcessTransit.c
+++ src/Hsm_ProcessTransit.c
@@ -1,1 +1,21 @@
 #include "Hsm_ProcessTransit.h"
+#include <string.h>
+
+HsmResult Hsm_ProcessTransit(Hsm *state_machine, HsmTransit *transit)
+{
+    HsmResult rval = 0;
+    Hsm *leaf;
+    HsmEvent event = HSM_EVENT_INIT;
+
+    if (state_machine && transit)
+    {
+        leaf = state_machine;
+
+        if (((leaf = state_machine->active)) && leaf->handler)
+        {
+            memset(transit,0,sizeof(*transit));
+            leaf->handler(leaf,&event,transit);
+        }
+    }
+    return 0;
+}
--- test/test_Hsm_ProcessTransit.c
+++ test/test_Hsm_ProcessTransit.c
@@ -3,16 +3,16 @@
 #include "Hsm_ProcessTransit.h"
 #include "mock_Hsm_DoTransit.h"
 
-static HsmEventResult m_handler(Hsm *self, HsmEvent *event, HsmTransit *transit);
+static HsmHandlerReturn m_handler(Hsm *self, HsmEvent *event, HsmTransit *transit);
 
 static unsigned m_hitcount;
 static unsigned m_loop;
 static Hsm m_hsm = { .handler = &m_handler };
-static Hsm m_hsm2 = { .parent = &m_hsm };
+static Hsm m_hsm2 = { .active = &m_hsm };
 static HsmTransit m_transit = { .target = &m_hsm };
 static HsmTransit m_transit2 = { .target = &m_hsm2 };
 
-static HsmEventResult m_handler(Hsm *self, HsmEvent *event, HsmTransit *transit) 
+static HsmHandlerReturn m_handler(Hsm *self, HsmEvent *event, HsmTransit *transit) 
 {
     if (event && *event == HSM_EVENT_INIT) 
     {
@@ -41,8 +41,8 @@ void tearDown(void)
  */
 void test_Hsm_ProcessTransit_HappyPath(void)
 {
-    Hsm_DoTransit_ExpectAndReturn(&m_hsm,&m_transit,0);
-    TEST_ASSERT_EQUAL(0,test_Hsm_ProcessTransit(&m_hsm,*m_transit));
+    Hsm_DoTransit_ExpectAndReturn(&m_hsm2,&m_transit,0);
+    TEST_ASSERT_EQUAL(0,Hsm_ProcessTransit(&m_hsm2,&m_transit));
     TEST_ASSERT_EQUAL(1,m_hitcount);
 }
 
@@ -54,7 +54,7 @@ void test_Hsm_ProcessTransit_InitTransits(void)
     m_loop = 1;
     Hsm_DoTransit_ExpectAndReturn(&m_hsm,&m_transit,0);
     Hsm_DoTransit_ExpectAndReturn(&m_hsm,&m_transit,0);
-    TEST_ASSERT_EQUAL(0,test_Hsm_ProcessTransit(&m_hsm,&m_transit));
+    TEST_ASSERT_EQUAL(0,Hsm_ProcessTransit(&m_hsm,&m_transit));
     TEST_ASSERT_EQUAL(2,m_hitcount);
 }
 
@@ -64,7 +64,7 @@ void test_Hsm_ProcessTransit_InitTransits(void)
 void test_Hsm_ProcessTransit_TransitFailReturned(void)
 {
     Hsm_DoTransit_ExpectAndReturn(&m_hsm,&m_transit,73);
-    TEST_ASSERT_EQUAL(73,test_Hsm_ProcessTransit(&m_hsm,&m_transit));
+    TEST_ASSERT_EQUAL(73,Hsm_ProcessTransit(&m_hsm,&m_transit));
     TEST_ASSERT_EQUAL(0,m_hitcount);
 }
 
@@ -73,7 +73,7 @@ void test_Hsm_ProcessTransit_TransitFailReturned(void)
  */
 void test_Hsm_ProcessTransit_NullStateFails(void)
 {
-    TEST_ASSERT_EQUAL(HSM_RESULT_PROCESS_TRANSIT_ERROR_NULL_STATE,test_Hsm_ProcessTransit(NULL,&m_transit));
+    TEST_ASSERT_EQUAL(HSM_RESULT_PROCESS_TRANSIT_ERROR_NULL_STATE,Hsm_ProcessTransit(NULL,&m_transit));
 }
 
 /*
@@ -81,6 +81,6 @@ void test_Hsm_ProcessTransit_NullStateFails(void)
  */
 void test_Hsm_ProcessTransit_NullTransitFails(void)
 {
-    TEST_ASSERT_EQUAL(HSM_RESULT_PROCESS_TRANSIT_ERROR_NULL_TRANSIT,test_Hsm_ProcessTransit(&m_hsm,NULL));
+    TEST_ASSERT_EQUAL(HSM_RESULT_PROCESS_TRANSIT_ERROR_NULL_TRANSIT,Hsm_ProcessTransit(&m_hsm,NULL));
 }
 
